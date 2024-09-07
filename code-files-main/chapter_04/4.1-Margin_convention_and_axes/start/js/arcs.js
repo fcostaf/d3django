842ebeffa3be23a186187aadc7d8a42a7a831f5d
fcostaf <fcostaf@yahoo.com.br>
2024-09-07 18:15:01 -0300

    cap 4 d3

--- code-files-main/chapter_04/4.1-Margin_convention_and_axes/start/js/arcs.js
+++ code-files-main/chapter_04/4.1-Margin_convention_and_axes/start/js/arcs.js
@@ -1,7 +1,65 @@
 // Load the data here
-
+d3.csv('./data/daily_precipitations.csv', d3.autoType).then(data => {
+    console.log('precipitation data', data);
+    drawArc(data);
+});
 
 // Draw the arc here
 const drawArc = (data) => {
+    const pieChartWidth = 300;
+    const pieChartHeight = 300;
+    const svg = d3.select('#arc')
+        .append('svg')
+        .attr('viewBox', [0, 0, pieChartWidth, pieChartHeight])
 
+    const innerChart = svg
+        .append('g')
+        .attr('transform', `translate(${pieChartWidth / 2}, ${pieChartHeight / 2})`)
+
+    const numberOfDays = data.length;
+    const numberOfDaysWithPrecipitation = data.filter(d => d.total_precip_in > 0).length;
+    const percentageDaysWithPrecipitation = Math.round(numberOfDaysWithPrecipitation / numberOfDays * 100);
+    const angleDaysWithPrecipitation_deg = percentageDaysWithPrecipitation * 360 / 100;
+    const angleDaysWithPrecipitation_rad = angleDaysWithPrecipitation_deg * Math.PI / 180;
+
+    const arcGenereator = d3.arc()
+        .innerRadius(80)
+        .outerRadius(120)
+        .padAngle(0.02)
+        .cornerRadius(6);
+
+    innerChart
+        .append('path')
+            .attr('d', () => {
+                return arcGenereator({
+                    startAngle: 0,
+                    endAngle: angleDaysWithPrecipitation_rad
+                })
+            })
+            .attr('fill', '#6EB7C2');
+    innerChart
+        .append('path')
+            .attr('d', () => {
+                return arcGenereator({
+                    startAngle: angleDaysWithPrecipitation_rad,
+                    endAngle: Math.PI * 2
+                })
+            })
+            .attr('fill', '#DCE2E2');
+
+    const centroid = arcGenereator
+            .startAngle(0)
+            .endAngle(angleDaysWithPrecipitation_rad)
+            .centroid();
+    
+    innerChart
+            .append('text')
+                .text(d => d3.format('.0%')(percentageDaysWithPrecipitation / 100))
+                .attr('x', centroid[0])
+                .attr('y', centroid[1])
+                .attr('text-anchor', 'middle')
+                .attr('dominant-baseline', 'middle')
+                .attr('fill', 'white')
+                .style('font-weight', 500)
+    
 };
--- code-files-main/chapter_04/4.1-Margin_convention_and_axes/start/js/line-chart.js
+++ code-files-main/chapter_04/4.1-Margin_convention_and_axes/start/js/line-chart.js
@@ -1,6 +1,139 @@
-// Load the data here
-
+d3.csv('./data/weekly_temperature.csv', d3.autoType).then(data => {
+    console.log('temperature data', data);
+    drawLineChart(data);
+    
+});
 // Create the line chart here
 const drawLineChart = (data) => {
+    const margin = {top: 40, bottom: 25, right: 170, left: 40};
+    const width = 1000;
+    const height = 500;
+    const innerWidth = width - margin.right - margin.left;
+    const innerHeight = height - margin.top - margin.bottom;
 
+    const svg = d3.select('#line-chart')
+        .append('svg')
+            .attr('viewBox', `0, 0, ${width}, ${height}`)
+
+    const innerChart = svg
+        .append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    
+    const firstDate = new Date(2021, 0o0, 0o1, 0, 0, 0);
+    const lastDate = d3.max(data, d => d.date);
+    const xScale = d3.scaleTime()
+        .domain([firstDate, lastDate])
+        .range([0, innerWidth]);
+    
+    const maxTemp = d3.max(data, d => d.max_temp_F);
+    const yScale = d3.scaleLinear()
+        .domain([0, maxTemp])
+        .range([innerHeight, 0]);
+
+    const bottomAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.timeFormat('%b'));innerChart
+        .append('g')
+            .attr('class', 'axis-x')
+            .attr('transform', `translate(0, ${innerHeight})`)
+            .call(bottomAxis);
+
+    d3.selectAll('.axis-x text')
+        .attr('x', d => {
+            const currentMonth = d;
+            const nextMonth = new Date(2021, currentMonth.getMonth() + 1, 1);
+            return (xScale(nextMonth) - xScale(currentMonth)) / 2;
+        })
+        .attr('y', '10px')
+        .style('font-family', 'Roboto, sans-serif')
+        .style('font-size', '14px');
+    
+    const leftAxis = d3.axisLeft(yScale);
+    innerChart
+        .append('g')
+            .attr('class', 'axis-y')
+            .call(leftAxis);
+
+    d3.selectAll('.axis-y text')
+        .attr('x', '-14px')
+        .style('font-family', 'Roboto, sans-serif')
+        .style('font-size', '14px');
+
+    svg
+        .append('text')
+            .text('Temperature (°F)')
+            .attr('y', 20)
+    
+    const aubergine = '#75485E';
+    innerChart
+        .selectAll('circle')
+            .data(data)
+            .join('circle')
+                .attr('r', 4)
+                .attr('cx', d => xScale(d.date))
+                .attr('cy', d => yScale(d.avg_temp_F))
+                .attr('fill', aubergine);
+
+    const areaGenerator = d3.area()
+        .x(d => xScale(d.date))
+        .y0(d => yScale(d.min_temp_F))
+        .y1(d => yScale(d.max_temp_F))
+        .curve(d3.curveCatmullRom);
+    
+    innerChart
+        .append('path')
+            .attr('d', areaGenerator(data))
+            .attr('fill', aubergine)
+            .attr('fill-opacity', 0.2);
+
+    const curveGenerator = d3.line()
+        .x(d => xScale(d.date))
+        .y(d => yScale(d.avg_temp_F))
+        .curve(d3.curveCatmullRom);
+
+    innerChart
+        .append('path')
+            .attr('d', curveGenerator(data))
+            .attr('fill', 'none')
+            .attr('stroke', aubergine);
+
+    innerChart
+        .append('text')
+            .text('Average temperature')
+            .attr('x', xScale(lastDate) + 10)
+            .attr('y', yScale(data[data.length - 1].avg_temp_F))
+            .attr('dominant-baseline', 'middle')
+            .attr('fill', aubergine);
+
+    innerChart
+        .append('text')
+            .text('Minimum temperature')
+            .attr('x', xScale(data[data.length - 3].date) + 13)
+            .attr('y', yScale(data[data.length - 3].min_temp_F) + 20)
+            .attr('alignment-baseline', 'hanging')
+            .attr('fill', aubergine);
+
+    innerChart
+        .append('line')
+            .attr('x1', xScale(data[data.length - 3].date))
+            .attr('y1', yScale(data[data.length - 3].min_temp_F) + 3)
+            .attr('x2', xScale(data[data.length - 3].date) + 10)
+            .attr('y2', yScale(data[data.length - 3].min_temp_F) + 20)
+            .attr('stroke', aubergine)
+            .attr('stroke-width', 2);
+
+    innerChart
+        .append('text')
+            .text('Maximum temperature')
+            .attr('x', xScale(data[data.length - 4].date) + 13)
+            .attr('y', yScale(data[data.length - 4].max_temp_F) - 20)
+            .attr('fill', aubergine);
+
+    innerChart
+        .append('line')
+            .attr('x1', xScale(data[data.length - 4].date))
+            .attr('y1', yScale(data[data.length - 4].max_temp_F) - 3)
+            .attr('x2', xScale(data[data.length - 4].date) + 10)
+            .attr('y2', yScale(data[data.length - 4].max_temp_F) - 20)
+            .attr('stroke', aubergine)
+            .attr('stroke-width', 2);
 };
