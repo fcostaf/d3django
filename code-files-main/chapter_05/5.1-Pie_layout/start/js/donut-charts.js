8c9e4ce0a9e3e7a89df6c8fc45332f1ef6de76e0
fcostaf <fcostaf@yahoo.com.br>
2024-10-12 15:00:30 -0300

    121024

--- code-files-main/chapter_05/5.1-Pie_layout/start/js/donut-charts.js
+++ code-files-main/chapter_05/5.1-Pie_layout/start/js/donut-charts.js
@@ -1,5 +1,30 @@
 const drawDonutCharts = (data) => {
-  // Generate the donut charts here
+  const svg = d3.select('#donut')
+    .append('svg')
+      .attr('viewBox', `0 0 ${width} ${height}`);
 
-  
+  const donutContainers = svg
+    .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+    const years = [1975, 1995, 2013]
+    const formats = data.columns.filter(format =>
+      format !== 'year');
+    
+    years.forEach(year => {
+
+      const donutContainer = donutContainers
+        .append('g')
+          .attr('transform', `translate(${xScale(year)}, ${innerHeight/2})`);
+      
+      const yearData = data.find(d => d.year == year);
+      const formattedData = [];
+
+      formats.forEach(format => {
+        formattedData.push({ format: format,
+          sales: yearData[format] });
+      });
+    
+    
+    });
 };
